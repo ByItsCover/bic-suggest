a8b5b4842823950fde01c6f4dbbc5675ed8b3f6c
Kalamojo <kojo22444488888888@gmail.com>
2026-07-24 22:08:11 -0400

    Handling unauthorized users

--- src/suggest/user_query.ts
+++ src/suggest/user_query.ts
@@ -1,14 +1,19 @@
 import * as lancedb from "@lancedb/lancedb"
 import { NIL as NIL_UUID } from "uuid";
 import { toHex } from "../utils";
-import { UserResult } from "../types";
+import { UserAttributes, UserResult } from "../types";
 import logger from "../logger";
 
 
-const userQuery = async (user_id: string, usersTable: lancedb.Table) => {
+const userQuery = async (userAttributes: UserAttributes | null, usersTable: lancedb.Table) => {
     const default_user_id = toHex(NIL_UUID);
+    let id_query = `user_id = X'${default_user_id}')`;
+    if (userAttributes !== null) {
+        id_query = `user_id IN (X'${userAttributes.uid_hex}', X'${default_user_id}')`
+    }
+
     const tableRes: UserResult[] = await usersTable.query()
-        .where(`user_id IN (X'${user_id}', X'${default_user_id}')`)
+        .where(id_query)
         .select(["user_id", "tower_embedding"])
         .limit(2)
         .toArray();
@@ -19,7 +24,8 @@ const userQuery = async (user_id: string, usersTable: lancedb.Table) => {
     if (tableRes.length == 1)
         return tableRes[0];
 
-    const user_object = tableRes.find(user => user.user_id === user_id);
+    const user_object = userAttributes === null ? undefined
+        : tableRes.find(user => user.user_id === userAttributes.uid_hex);
     if (user_object === undefined) {
         throw new Error("Both default user and current user do not yet exist");
     }
