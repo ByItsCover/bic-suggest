7eaffc72592dad9bce4fd23f8353c56bb57063b8
Kalamojo <kojo22444488888888@gmail.com>
2026-07-27 18:14:43 -0400

    Handling missing tables

--- src/suggest/suggest.ts
+++ src/suggest/suggest.ts
@@ -2,8 +2,8 @@ import { RequestContext } from "@aws-lambda-powertools/event-handler/types";
 import * as lancedb from "@lancedb/lancedb";
 import { userDetails, userRatings } from "./user_query";
 import vectorSearch from "./vector_search";
+import { CoverResult, UserAttributes } from "../types";
 import logger from "../logger";
-import { UserAttributes } from "../types";
 
 
 const suggest = async (reqCtx : RequestContext) => {
@@ -11,16 +11,22 @@ const suggest = async (reqCtx : RequestContext) => {
     logger.info('Printing body of request');
     logger.info(JSON.stringify(body));
 
+    let suggestResults: CoverResult[] = [];
+
     const userAttributes = reqCtx.get("user_attributes") as UserAttributes | null;
-    const usersTable = reqCtx.get("users_table") as lancedb.Table;
-    const coversTable = reqCtx.get("covers_table") as lancedb.Table;
-    const feedbackTable = reqCtx.get("feedback_table") as lancedb.Table;
+    const usersTable = reqCtx.get("users_table") as lancedb.Table | null;
+    const coversTable = reqCtx.get("covers_table") as lancedb.Table | null;
+    const feedbackTable = reqCtx.get("feedback_table") as lancedb.Table | null;
 
-    const userObject = await userDetails(userAttributes, usersTable);
-    const suggestResults = await vectorSearch(userObject.tower_embedding, coversTable);
+    if (usersTable === null || coversTable === null) {
+        logger.info("User and/or cover table have yet to be created. Returning empty results");
+    } else {
+        const userObject = await userDetails(userAttributes, usersTable);
+        suggestResults = await vectorSearch(userObject.tower_embedding, coversTable);
 
-    if (userAttributes !== null) {
-        await userRatings(suggestResults, userAttributes, feedbackTable);
+        if (userAttributes !== null && feedbackTable !== null) {
+            await userRatings(suggestResults, userAttributes, feedbackTable);
+        }
     }
 
     return {
