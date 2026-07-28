6ca0c9ca1e99f7dc69e29fc7a4b19339bfe048eb
Kalamojo <kojo22444488888888@gmail.com>
2026-07-28 17:39:02 -0400

    Using correct uid type

--- src/utils.ts
+++ src/utils.ts
@@ -8,4 +8,18 @@ const toBytes = (uuid: string) => {
     return parse(uuid);
 }
 
-export { toHex, toBytes };
+const idsAreEqual = (uid1: Uint8Array, uid2: Uint8Array) => {
+    if (uid1.length != uid2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < uid1.length; i++) {
+        if (uid1[i] != uid2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+export { toHex, toBytes, idsAreEqual };
